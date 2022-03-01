35f804dce5b97f09cb117dab07be9feea8eaa91d
ybdong919 <ybdong919@gmail.com>
2022-03-01 16:10:09 -0500

    Web database 3/132022

---
+++ WebDatabase/Ocean/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+# Register your models here.
+#from .models import Codeathon
+
+#class CodeathonAdmin(admin.ModelAdmin):
+ #    list_display = ('accession',)
+
+#admin.site.register(Codeathon, CodeathonAdmin)
